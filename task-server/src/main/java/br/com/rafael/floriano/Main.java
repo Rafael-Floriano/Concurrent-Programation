9f6bacbf70eb79d8f5d792515d74b33d0cab04a8
Rafael-Floriano <rafael.florianoc9@gmail.com>
2024-08-26 23:32:14 -0300

    feat: implemented ServerSocket in main class

--- task-server/src/main/java/br/com/rafael/floriano/Main.java
+++ task-server/src/main/java/br/com/rafael/floriano/Main.java
@@ -1,7 +1,22 @@
 package br.com.rafael.floriano;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("--- Initializing Socket Server ---!");
+
+        try {
+            ServerSocket serverSocket = new ServerSocket(12345);
+            while (true) {
+                Socket socket = serverSocket.accept();
+                System.out.println("Accept conection in port: " + socket.getPort());
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
     }
 }
---
+++ task-server/src/main/java/br/com/rafael/floriano/client/ClientTask.java
@@ -0,0 +1,23 @@
+package br.com.rafael.floriano.client;
+
+import java.io.IOException;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+public class ClientTask {
+
+    public static void main(String[] args) {
+
+        try {
+            Socket socket = new Socket("localhost", 12345);
+            System.out.println("Conection establish'");
+            socket.close();
+        } catch (UnknownHostException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+}
