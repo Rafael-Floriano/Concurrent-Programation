20bd550ca4c01491e41ac05ff03d48abe61449ef
Rafael-Floriano <rafael.florianoc9@gmail.com>
2024-09-01 21:37:49 -0300

    refact: change roughty implementation of SocketServer to specialist class CustomerServerSocket

--- task-server/src/main/java/br/com/rafael/floriano/Main.java
+++ task-server/src/main/java/br/com/rafael/floriano/Main.java
@@ -1,22 +1,11 @@
 package br.com.rafael.floriano;
 
-import java.io.IOException;
-import java.net.ServerSocket;
-import java.net.Socket;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("--- Initializing Socket Server ---!");
 
-        try {
-            ServerSocket serverSocket = new ServerSocket(12345);
-            while (true) {
-                Socket socket = serverSocket.accept();
-                System.out.println("Accept conection in port: " + socket.getPort());
-            }
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+        CustomerServerSocket customerServerSocket = new CustomerServerSocket();
+        customerServerSocket.init();
 
     }
 }
