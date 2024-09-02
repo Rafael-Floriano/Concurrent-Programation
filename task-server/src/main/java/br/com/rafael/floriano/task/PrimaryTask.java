9d6b29cf03dcc3761748328be74b79921d03510d
Rafael-Floriano <rafael.florianoc9@gmail.com>
2024-09-01 21:39:03 -0300

    feat: created new class to execute primary tasks of clients in server socket

--- task-server/src/main/java/br/com/rafael/floriano/task/PrimaryTask.java
+++ task-server/src/main/java/br/com/rafael/floriano/task/PrimaryTask.java
@@ -1,4 +1,22 @@
 package br.com.rafael.floriano.task;
 
-public class PrimaryTask {
+import java.net.Socket;
+
+public class PrimaryTask implements Runnable {
+
+    private Socket socket;
+
+    @Override
+    public void run() {
+        System.out.println("Working in primary task of client: " + socket);
+        try {
+            Thread.sleep(20000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public PrimaryTask(Socket socket) {
+        this.socket = socket;
+    }
 }
