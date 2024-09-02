e0c929ff66bf33dcb5b6e9cb05517786f39fa669
Rafael-Floriano <rafael.florianoc9@gmail.com>
2024-09-01 21:38:23 -0300

    refact: change name of clientTask to client

--- task-server/src/main/java/br/com/rafael/floriano/client/Client.java
+++ task-server/src/main/java/br/com/rafael/floriano/client/Client.java
@@ -3,14 +3,20 @@ package br.com.rafael.floriano.client;
 import java.io.IOException;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.util.Scanner;
 
-public class ClientTask {
+public class Client {
 
     public static void main(String[] args) {
 
         try {
             Socket socket = new Socket("localhost", 12345);
             System.out.println("Conection establish'");
+
+            Scanner scanner = new Scanner(System.in);
+
+            scanner.next();
+
             socket.close();
         } catch (UnknownHostException e) {
             throw new RuntimeException(e);
