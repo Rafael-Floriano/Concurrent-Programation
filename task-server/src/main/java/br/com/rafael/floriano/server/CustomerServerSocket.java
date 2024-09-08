f90c1f9cd92a957b859829e67ebe598027eec1af
Rafael-Floriano <rafael.florianoc9@gmail.com>
2024-09-08 15:11:27 -0300

    refact: Adding comments to TCP server initialization methods

--- task-server/src/main/java/br/com/rafael/floriano/server/CustomerServerSocket.java
+++ task-server/src/main/java/br/com/rafael/floriano/server/CustomerServerSocket.java
@@ -17,6 +17,9 @@ import java.util.concurrent.Executors;
  */
 public class CustomerServerSocket {
 
+    /**
+     * Starts the server without a specified thread size, we can create as many as we want
+     * */
     public void init() {
         System.out.println("--- Initializing Socket Server ---!");
         try {
@@ -31,6 +34,14 @@ public class CustomerServerSocket {
         }
     }
 
+    /**
+     * initWithFixedPoolOfThreads create a pool of threads with
+     * specific size through Executors class with newFixedThreadPool.
+     * "newFixedThreadPool" threads in a non-horizontally scalable way,
+     * the system will always have this specific number of threads to work with when we refer to the specific pool created by the executor,
+     * if new requests are made to the server and all threads are busy, the client will enter a waiting mode, where when a new thread becomes free,
+     * the client request that was waiting will be processed normally
+     */
     public void initWithFixedPoolOfThreads() {
 
         ExecutorService threadsPool = Executors.newFixedThreadPool(2);
@@ -47,6 +58,13 @@ public class CustomerServerSocket {
         }
     }
 
+    /**
+     * This method initializes the server with a thread pool determined as "newCachedThreadPool"
+     * which means that we can always call the pool to obtain a new thread,
+     * unlike the "newFixedThreadPool" method if all created threads are busy,
+     * the pool can create a new thread to take care of the new requested process,
+     * the threads created during this pool will be removed if they are not used within 60 seconds
+     * */
     public void initWithCachedThreads() {
 
         ExecutorService threadsPool = Executors.newCachedThreadPool();
