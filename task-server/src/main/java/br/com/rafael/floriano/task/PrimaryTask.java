a68ee878568edb645d4c305b02b408a3ea98eef3
Rafael-Floriano <rafael.florianoc9@gmail.com>
2024-09-12 23:46:54 -0300

    fix: created new method to make logic of commands in tcp server

--- task-server/src/main/java/br/com/rafael/floriano/task/PrimaryTask.java
+++ task-server/src/main/java/br/com/rafael/floriano/task/PrimaryTask.java
@@ -1,6 +1,9 @@
 package br.com.rafael.floriano.task;
 
+import java.io.IOException;
+import java.io.PrintStream;
 import java.net.Socket;
+import java.util.Scanner;
 
 /**
  * PrimaryTask emulates a heavy task in the server
@@ -13,10 +16,28 @@ public class PrimaryTask implements Runnable {
     @Override
     public void run() {
         System.out.println("Working in primary task of client: " + socket);
+        readingClientCommands();
+    }
+
+    private void readingClientCommands() {
         try {
-            Thread.sleep(20000);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
+            Scanner scanner = new Scanner(socket.getInputStream());
+            PrintStream outputClient = new PrintStream(socket.getOutputStream());
+            while (scanner.hasNextLine()) {
+                final String command = scanner.nextLine();
+                switch (command) {
+                    case "c1":
+                        outputClient.print("c1 command executed");
+                        break;
+                    case "c2":
+                        outputClient.print("c2 command executed");
+                        break;
+                    default:
+                        outputClient.print("Not exists this command: " + command);
+                }
+            }
+        } catch (IOException e) {
+            System.out.println("Server cannot read");
         }
     }
 
