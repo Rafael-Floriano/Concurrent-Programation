30362a23f575e4d6db8d2192c3137b9e503ac422
Rafael-Floriano <rafael.florianoc9@gmail.com>
2024-09-12 23:47:20 -0300

    feat: created new class 'SingleClientConnection' to start a single client connection

---
+++ task-server/src/main/java/br/com/rafael/floriano/client/SingleClientConnection.java
@@ -0,0 +1,9 @@
+package br.com.rafael.floriano.client;
+
+public class SingleClientConnection {
+
+    public static void main(String[] args) {
+        Client client = new Client();
+        client.makeConnection();
+    }
+}
