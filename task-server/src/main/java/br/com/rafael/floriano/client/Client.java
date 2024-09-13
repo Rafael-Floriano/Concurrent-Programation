b950f5dd131543341c8fdee1756457c20a84a195
Rafael-Floriano <rafael.florianoc9@gmail.com>
2024-09-12 23:20:20 -0300

    refact: splited the logic of client in more methods to make code more clean

--- task-server/src/main/java/br/com/rafael/floriano/client/Client.java
+++ task-server/src/main/java/br/com/rafael/floriano/client/Client.java
@@ -1,8 +1,8 @@
 package br.com.rafael.floriano.client;
 
 import java.io.IOException;
+import java.io.PrintStream;
 import java.net.Socket;
-import java.net.UnknownHostException;
 import java.util.Scanner;
 
 /**
@@ -13,20 +13,56 @@ import java.util.Scanner;
 public class Client implements Runnable {
 
 
-    public static void makeConnection() {
+    public void makeConnection() {
+        final Socket socket = getConnectionWithTcpServer();
+        new Thread(() -> createTcpOutputConnection(socket)).start();
+        new Thread(() -> createTcpInputConnection(socket)).start();
+    }
 
+    private Socket getConnectionWithTcpServer() {
         try {
             Socket socket = new Socket("localhost", 12345);
             System.out.println("Conection establish'");
-            Scanner scanner = new Scanner(System.in);
-            scanner.next();
-            socket.close();
-        } catch (UnknownHostException e) {
-            throw new RuntimeException(e);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+            return socket;
+        } catch (Exception exception) {
+            throw new RuntimeException("Error to get connection with tcp server - ",exception);
         }
+    }
 
+    private void createTcpOutputConnection(final Socket socket) {
+        try {
+            PrintStream output = new PrintStream(socket.getOutputStream());
+            Scanner scanner = new Scanner(System.in);
+            while (scanner.hasNext()) {
+                final String command = scanner.next();
+                if (command.trim().equals(":q")) {
+                    closeSocketConnection(socket);
+                    break;
+                }
+                output.println(scanner.next());
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("outputTask - ",e);
+        }
+    }
+
+    private void createTcpInputConnection(final Socket socket) {
+        try {
+            Scanner scanner = new Scanner(socket.getInputStream());
+            while (scanner.hasNext()) {
+                System.out.println("SERVER RESPONSE - " + scanner.next());
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("outputTask - ",e);
+        }
+    }
+
+    private void closeSocketConnection(final Socket socket) {
+        try {
+            socket.close();
+        } catch (IOException e) {
+            throw new RuntimeException("Error to close socket connection - ",e);
+        }
     }
 
     @Override
